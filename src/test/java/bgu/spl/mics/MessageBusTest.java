8005ae9bd96c4094e48e6cf312b72198906ad0e4
tsuri <tsurif@post.bgu.ac.il>
2020-11-25 20:19:39 +0200

    added Massagebus tests class

---
+++ src/test/java/bgu/spl/mics/MessageBusTest.java
@@ -0,0 +1,40 @@
+package bgu.spl.mics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageBusTest {
+
+    @Test
+    void subscribeEvent() {
+    }
+
+    @Test
+    void subscribeBroadcast() {
+    }
+
+    @Test
+    void complete() {
+    }
+
+    @Test
+    void sendBroadcast() {
+    }
+
+    @Test
+    void sendEvent() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void unregister() {
+    }
+
+    @Test
+    void awaitMessage() {
+    }
+}
