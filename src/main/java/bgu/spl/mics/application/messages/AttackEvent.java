76ed18e6761567a2f6a6bbbb01ede4143efd976c
rozenbergidan <idanroze@post.bgu.ac.il>
2020-12-03 21:19:52 +0200

    added fields to attackEvent

--- src/main/java/bgu/spl/mics/application/messages/AttackEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/AttackEvent.java
@@ -1,6 +1,17 @@
 package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
+import bgu.spl.mics.application.passiveObjects.Attack;
+
+import java.util.List;
 
 public class AttackEvent implements Event<Boolean> {
-	
+	public boolean expectedResult;
+	public List<Integer> ewoks;
+
+	public AttackEvent(List<Integer> ewoks){
+	    expectedResult=true;
+	    this.ewoks=ewoks;
+
+    }
+
 }
