9bd8025df8a6b3a3ad6b101f3cb59aeb02a4c713
rozenbergidan <idanroze@post.bgu.ac.il>
2020-12-05 19:01:33 +0200

    added hanSolo and C3PO init

--- src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/C3POMicroservice.java
@@ -1,24 +1,32 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.Callback;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.AttackEvent;
 
 
 /**
- * C3POMicroservices is in charge of the handling {@link AttackEvents}.
+ * C3POMicroservices is in charge of the handling {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class C3POMicroservice extends MicroService {
-	
+
+    private Callback<AttackEvent> attackCallBack= new Callback() {
+        @Override
+        public void call(Object c) {
+            //TODO: complete this
+        }
+    };
     public C3POMicroservice() {
         super("C3PO");
     }
 
     @Override
     protected void initialize() {
-
+        subscribeEvent(AttackEvent.class,attackCallBack);
     }
 }
--- src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
+++ src/main/java/bgu/spl/mics/application/services/HanSoloMicroservice.java
@@ -1,17 +1,28 @@
 package bgu.spl.mics.application.services;
 
 
+import bgu.spl.mics.Callback;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.AttackEvent;
+
+import java.util.Calendar;
 
 /**
- * HanSoloMicroservices is in charge of the handling {@link AttackEvents}.
+ * HanSoloMicroservices is in charge of the handling {@link AttackEvent}.
  * This class may not hold references for objects which it is not responsible for:
- * {@link AttackEvents}.
+ * {@link AttackEvent}.
  *
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class HanSoloMicroservice extends MicroService {
+
+    private Callback<AttackEvent> attackCallBack= new Callback() {
+        @Override
+        public void call(Object c) {
+            //TODO: complete this
+        }
+    };
 
     public HanSoloMicroservice() {
         super("Han");
@@ -20,6 +31,7 @@ public class HanSoloMicroservice extends MicroService {
 
     @Override
     protected void initialize() {
+        subscribeEvent(AttackEvent.class,attackCallBack);
 
     }
 }
