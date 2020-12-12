b20433a99bc39a09caecc043bc7b0589152c23b0
tsuri <tsurif@post.bgu.ac.il>
2020-12-12 22:29:04 +0200

    decumentation

--- src/main/java/bgu/spl/mics/application/messages/BombDestroyerEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/BombDestroyerEvent.java
@@ -1,7 +1,15 @@
 package bgu.spl.mics.application.messages;
 
 import bgu.spl.mics.Event;
+import bgu.spl.mics.Message;
 
+
+/**
+ * Implement Event, this Event is for R2D2 use only.
+ * Once all the futures of the Attack-Events that leia sent have resolved,
+ * leia send this event.
+ * @expectedResult the result this Event will have once resolved
+ */
 public class BombDestroyerEvent implements Event<Boolean> {
     public boolean expectedResult;
     public BombDestroyerEvent(){
