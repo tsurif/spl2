797c4218fb632febdeaa9f77d9febb2fba4213dc
rozenbergidan <idanroze@post.bgu.ac.il>
2020-12-13 00:08:58 +0200

    changed the implementation of PostOffice

--- src/main/java/bgu/spl/mics/PostOffice.java
+++ src/main/java/bgu/spl/mics/PostOffice.java
@@ -3,12 +3,20 @@ package bgu.spl.mics;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class PostOffice {
-    private Queue<BlockingQueue<Message>> mailBoxes;
-
+    //private Queue<BlockingQueue<Message>> mailBoxes;
+    private LinkedList<BlockingQueue<Message>> mailBoxes;
+    private AtomicInteger counter;
+    private AtomicInteger size;
     public PostOffice(){
         mailBoxes = new LinkedList<>();
+        counter=new AtomicInteger();
+        size=new AtomicInteger();
+        counter.set(0);
+        size.set(0);
     }
 
     public synchronized int size(){
@@ -19,11 +27,16 @@ public class PostOffice {
     }
 
     public synchronized void remove(BlockingQueue<Message> q){
-        if (mailBoxes.contains(q)) mailBoxes.remove(q);
+        if (mailBoxes.contains(q)) {
+            mailBoxes.remove(q);
+            size.getAndDecrement();
+        }
     }
 
     public synchronized void add(BlockingQueue<Message> subscriber){
         mailBoxes.add(subscriber);
+        size.getAndIncrement();
+
     }
     public synchronized void sendBrodcast(Broadcast b){
         for (BlockingQueue<Message> mailBox:mailBoxes) {
@@ -32,8 +45,12 @@ public class PostOffice {
     }
 
     public synchronized void sendEvant(Event e){
-        BlockingQueue<Message> msQueue = mailBoxes.remove();
-        mailBoxes.add(msQueue);
+        counter.compareAndSet(size.get(),0);
+        int i=counter.getAndSet((counter.get()+1)% size.get());
+        BlockingQueue<Message>msQueue=mailBoxes.get(i);
         msQueue.add(e);
+//        BlockingQueue<Message> msQueue = mailBoxes.remove();
+//        mailBoxes.add(msQueue);
+//        msQueue.add(e);
     }
 }
