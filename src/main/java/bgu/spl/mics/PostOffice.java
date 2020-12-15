8cc0d22a3197a53942ed91e025264fb26670a6ac
tsuri <tsurif@post.bgu.ac.il>
2020-12-15 21:52:36 +0200

    postOffice docs

--- src/main/java/bgu/spl/mics/PostOffice.java
+++ src/main/java/bgu/spl/mics/PostOffice.java
@@ -4,6 +4,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 
+/**
+ * this is an Object that helps to manege the MessageBusImpl.
+ * the MessageBusImpl use this obj to manage the message passing between the Bus users.
+ * for every message type MessageBusImpl will use an postOffice instance.
+ * this is synchronized obj so every call for specific type of message will sync the messageBusImpl only for that type.
+ *
+ */
 public class PostOffice {
     private Queue<BlockingQueue<Message>> mailBoxes;
 
@@ -11,26 +18,55 @@ public class PostOffice {
         mailBoxes = new LinkedList<>();
     }
 
+    /**
+     * @return the number of MicroService register for the message-type this PostOffice store.
+     */
     public synchronized int size(){
         return mailBoxes.size();
     }
+
+    /**
+     *
+     * @return true if and only if no one is registered for the message-type this PostOffice store.
+     */
     public synchronized boolean isEmpty(){
         return mailBoxes.isEmpty();
     }
 
-    public synchronized void remove(BlockingQueue<Message> q){
-        if (mailBoxes.contains(q)) mailBoxes.remove(q);
+    /**
+     * remove the queue of a microService.
+     * use when this microService unregistered
+     * @param subscriber a queue of a MicroService to remove
+     */
+    public synchronized void remove(BlockingQueue<Message> subscriber){
+        if (mailBoxes.contains(subscriber)) mailBoxes.remove(subscriber);
     }
 
+    /**
+     * add the queue of a microService.
+     * use when this microService registered
+     * @param subscriber a queue of a MicroService to add
+     */
     public synchronized void add(BlockingQueue<Message> subscriber){
         mailBoxes.add(subscriber);
     }
+
+    /**
+     * send a Broadcast to all the MicroService that registered to this PostOffice.
+     * @param b Broadcast to send.
+     */
     public synchronized void sendBrodcast(Broadcast b){
         for (BlockingQueue<Message> mailBox:mailBoxes) {
             mailBox.add(b);
         }
     }
 
+    /**
+     * send an event to the first microService in the queue,
+     * the send it to the back
+     * supporting the round-robbin manner.
+     * @param e an event to pass to a microService.
+     */
     public synchronized void sendEvant(Event e){
         BlockingQueue<Message> msQueue = mailBoxes.remove();
         mailBoxes.add(msQueue);
